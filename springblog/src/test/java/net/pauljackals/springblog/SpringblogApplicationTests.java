5341ab4657ce413977633cc9495aff09f664aa84
Pawel Sakaluk <paul.jackals@gmail.com>
2021-11-06 21:39:38 +0100

    app init

---
+++ springblog/src/test/java/net/pauljackals/springblog/SpringblogApplicationTests.java
@@ -0,0 +1,13 @@
+package net.pauljackals.springblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
