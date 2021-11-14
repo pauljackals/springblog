381c3e71a6627bf738e46098e0e3badb9e8ab419
Pawel Sakaluk <paul.jackals@gmail.com>
2021-11-14 17:10:48 +0100

    added csv files

--- springblog/src/main/java/net/pauljackals/springblog/SpringblogApplication.java
+++ springblog/src/main/java/net/pauljackals/springblog/SpringblogApplication.java
@@ -2,8 +2,10 @@ package net.pauljackals.springblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:beans/*.xml")
 public class SpringblogApplication {
 
 	public static void main(String[] args) {
