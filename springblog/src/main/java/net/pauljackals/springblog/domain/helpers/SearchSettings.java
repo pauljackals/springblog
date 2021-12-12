90f7c18372c52ede461e40ae940292f65abe6dc8
Pawel Sakaluk <paul.jackals@gmail.com>
2021-12-12 13:23:07 +0100

    added search settings validation

--- springblog/src/main/java/net/pauljackals/springblog/domain/helpers/SearchSettings.java
+++ springblog/src/main/java/net/pauljackals/springblog/domain/helpers/SearchSettings.java
@@ -1,6 +1,7 @@
 package net.pauljackals.springblog.domain.helpers;
 
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +9,17 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class SearchSettings {
+    @Size(max = 32, message = "tag must be no longer than 32 characters")
+    @Pattern(regexp = "^|\\S+$", message = "tag must not contain any whitespace characters")
     private String tag;
+
+    @Pattern(regexp = "^|\\w+( \\w+)*$", message = "usernames contain only letters and numbers")
     private String authors;
+
+    @Size(max = 128, message = "word must be no longer than 128 characters")
+    @Pattern(regexp = "^|\\S+$", message = "word must not contain any whitespace characters")
     private String word;
+
     private Sorting sortByComments;
     private Sorting sortByAuthors;
     private Sorting sortByContent;
--- springblog/src/main/java/net/pauljackals/springblog/domain/helpers/UsernameFilter.java
+++ springblog/src/main/java/net/pauljackals/springblog/domain/helpers/UsernameFilter.java
@@ -1,5 +1,6 @@
 package net.pauljackals.springblog.domain.helpers;
 
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import lombok.Data;
@@ -9,5 +10,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UsernameFilter {
     @Size(max = 32, message = "usernames are not longer than 32 characters")
+    @Pattern(regexp = "^|\\w+$", message = "usernames contain only letters and numbers")
     String username;
 }
