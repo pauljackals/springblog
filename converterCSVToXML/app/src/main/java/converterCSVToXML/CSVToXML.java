d09918f0c6c8c18f89364e10358a8215150fb593
Pawel Sakaluk <paul.jackals@gmail.com>
2021-12-14 12:25:12 +0100

    cleaned converter up

--- converterCSVToXML/app/src/main/java/converterCSVToXML/CSVToXML.java
+++ converterCSVToXML/app/src/main/java/converterCSVToXML/CSVToXML.java
@@ -8,9 +8,7 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
