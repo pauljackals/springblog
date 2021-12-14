19e0e5110986f89f58101b9b16d161882cd8a034
Pawel Sakaluk <paul.jackals@gmail.com>
2021-12-14 13:58:50 +0100

    made beans.xml optional

--- springblog/src/main/java/net/pauljackals/springblog/SpringblogApplication.java
+++ springblog/src/main/java/net/pauljackals/springblog/SpringblogApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:beans.xml")
+@ImportResource("classpath*:beans.xml")
 public class SpringblogApplication {
 
 	public static void main(String[] args) {
