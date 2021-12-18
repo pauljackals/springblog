766fd47dff1e43074b53365145044240396bccd1
Pawel Sakaluk <paul.jackals@gmail.com>
2021-12-18 12:49:01 +0100

    added file upload exception handler

---
+++ springblog/src/main/java/net/pauljackals/springblog/controller/web/advice/FileUploadExceptionAdvice.java
@@ -0,0 +1,24 @@
+package net.pauljackals.springblog.controller.web.advice;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class FileUploadExceptionAdvice {
+    
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ModelAndView handleMaxSizeException(
+      MaxUploadSizeExceededException exc, 
+      HttpServletRequest request,
+      HttpServletResponse response
+    ) {
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.getModel().put("message", "upload too large");
+        return modelAndView;
+    }
+}
